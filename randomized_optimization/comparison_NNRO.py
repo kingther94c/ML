489421c80fa256fd83dd7caab4ec3bb19123608d
kelvin <kelvin.c@nyu.edu>
2021-03-14 20:54:31 -0400

    Randomized Optimization

--- randomized_optimization/comparison_NNRO.py
+++ randomized_optimization/comparison_NNRO.py
@@ -11,7 +11,7 @@ class NeuralNetwork(mlrose.NeuralNetwork):
     def __init__(self, hidden_nodes=None,
                  activation='relu',
                  algorithm='random_hill_climb',
-                 max_iters=100,
+                 max_iters=1000,
                  bias=True,
                  is_classifier=True,
                  learning_rate=0.1,
@@ -54,45 +54,42 @@ class NeuralNetwork(mlrose.NeuralNetwork):
 
 if __name__ == "__main__":
     scorer = make_scorer(balanced_accuracy_score)
-    # # MNIST
-    # print("===============================MNIST===============================")
-    # X, y = fetch_mnist()
-    # one_hot = OneHotEncoder(categories='auto')
-    # y = one_hot.fit_transform(y.reshape(-1, 1)).todense()
-    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=42)
-    # num_features = X_train.shape[0]
-    #
-    # # Select model
-    # model_candidates = {
-    #     "Gradient Descent": (mlrose.NeuralNetwork, {"hidden_nodes": [[64]]}),
-    #     "RHC": (mlrose.NeuralNetwork, {"hidden_nodes": [[64]],
-    #                                    "restarts": [100]}),
-    #     "SA": (mlrose.NeuralNetwork, {"hidden_nodes": [[64]],
-    #                                   "schedule": [mlrose.GeomDecay(init_temp=10.0, decay=0.99, min_temp=0.001),
-    #                                                mlrose.GeomDecay(init_temp=10.0, decay=0.9, min_temp=0.001),
-    #                                                mlrose.GeomDecay(init_temp=10.0, decay=0.8, min_temp=0.001)]}),
-    #     "GA": (mlrose.NeuralNetwork, {"hidden_nodes": [[64]],
-    #                                   "pop_size": [300],
-    #                                   "mutation_prob": [0.1, 0.3, 0.5, 0.7]}),
-    # }
-    # models = {k: select_models(v[0], v[1], X_train, y_train) for k, v in model_candidates.items()}
-    #
-    # for model_name, model in models.items():
-    #     print(model_name)
-    #     print(model)
-    #     start = time.time()
-    #     model.fit(X_train, y_train)
-    #     end = time.time()
-    #     print("Training Time", end-start)
-    #
-    #     print("Train:", scorer(model, X=X_train, y_true=y_train))
-    #     start = time.time()
-    #     print("Test:", scorer(model, X=X_test, y_true=y_test))
-    #     end = time.time()
-    #     print("Prediction Time", end - start)
-    #
-    # compare(X_train, X_test, y_train, y_test, models, dataset_name="MNIST", scorer=scorer, n_jobs=-3)
+    # MNIST
+    print("===============================MNIST===============================")
+    X, y = fetch_mnist()
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=42)
+    num_features = X_train.shape[0]
 
+    # Select model
+    model_candidates = {
+        "Gradient Descent": (NeuralNetwork, {"hidden_nodes": [[64]]}),
+        "RHC": (NeuralNetwork, {"hidden_nodes": [[64]],
+                                       "restarts": [20]}),
+        "SA": (NeuralNetwork, {"hidden_nodes": [[64]],
+                                      "schedule": [mlrose.GeomDecay(init_temp=10.0, decay=0.99, min_temp=0.001),
+                                                   mlrose.GeomDecay(init_temp=10.0, decay=0.9, min_temp=0.001),
+                                                   mlrose.GeomDecay(init_temp=10.0, decay=0.8, min_temp=0.001)]}),
+        "GA": (NeuralNetwork, {"hidden_nodes": [[64]],
+                                      "pop_size": [300],
+                                      "mutation_prob": [0.1, 0.3, 0.5, 0.7]}),
+    }
+    models = {k: select_models(v[0], v[1], X_train, y_train) for k, v in model_candidates.items()}
+
+    for model_name, model in models.items():
+        print(model_name)
+        print(model)
+        start = time.time()
+        model.fit(X_train, y_train)
+        end = time.time()
+        print("Training Time", end-start)
+
+        print("Train:", scorer(model, X=X_train, y_true=y_train))
+        start = time.time()
+        print("Test:", scorer(model, X=X_test, y_true=y_test))
+        end = time.time()
+        print("Prediction Time", end - start)
+
+    compare(X_train, X_test, y_train, y_test, models, dataset_name="MNIST", scorer=scorer, n_jobs=-2)
 
     print("===============================Wine Quality===============================")
     X, y = fetch_wine()
@@ -103,7 +100,7 @@ if __name__ == "__main__":
     model_candidates = {
         "Gradient Descent": (NeuralNetwork, {"hidden_nodes": [[4, 16]]}),
         "RHC": (NeuralNetwork, {"hidden_nodes": [[4, 16]],
-                                       "restarts": [100]}),
+                                       "restarts": [20]}),
         "SA": (NeuralNetwork, {"hidden_nodes": [[4, 16]],
                                       "schedule": [mlrose.GeomDecay(init_temp=10.0, decay=0.99, min_temp=0.001),
                                                    mlrose.GeomDecay(init_temp=10.0, decay=0.9, min_temp=0.001),
@@ -128,4 +125,4 @@ if __name__ == "__main__":
         end = time.time()
         print("Prediction Time", end - start)
 
-    compare(X_train, X_test, y_train, y_test, models, dataset_name="Wine Quality", scorer=scorer, n_jobs=-3)
+    compare(X_train, X_test, y_train, y_test, models, dataset_name="Wine Quality", scorer=scorer, n_jobs=-2)
